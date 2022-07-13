efc3231644a358b1794ddb4323a2d2abfc9a7152
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-07-13 09:05:16 -0300

    configs

--- Data/dbContext.cs
+++ Data/dbContext.cs
@@ -9,5 +9,7 @@ namespace storeproject.Data
         { }
         public DbSet<Users>? User { get; set; }
         public DbSet<Driver>? Driver { get; set; }
+        public DbSet<storeproject.Models.ClientAdress>? ClientAdress { get; set; }
+        public DbSet<storeproject.Models.Client>? Client { get; set; }
     }
 }
