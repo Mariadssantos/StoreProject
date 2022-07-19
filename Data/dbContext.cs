5bd6287a0a3af72ba24f51e76bbf0ebf1911be45
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-07-19 19:25:11 -0300

    Config db

--- Data/dbContext.cs
+++ Data/dbContext.cs
@@ -11,6 +11,8 @@ namespace storeproject.Data
         public DbSet<Driver>? Driver { get; set; }
         public DbSet<Product>? Product { get; set; }
         public DbSet<RequestItem>? RequestItem { get; set; }
+        public DbSet<OrderFile>? OrderFile { get; set; }
+        public DbSet<Delivery>? Delivery { get; set; }
         public DbSet<storeproject.Models.ClientAdress>? ClientAdress { get; set; }
         public DbSet<storeproject.Models.Client>? Client { get; set; }
     }
