7ba0d40c22fc9c99559a1bfa5aa68af8f02bfcb9
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-07-19 18:22:13 -0300

    create Product

---
+++ Migrations/20220718212629_Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace storeproject.Migrations
+{
+    public partial class Product : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateRegister = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace storeproject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+        public string  Status { get; set; }
+        public DateTime DateRegister { get; set; }
+    }
+}
