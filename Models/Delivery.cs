46617ed1d31909a714dd3e81c629053d85f9c4ba
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-07-19 19:24:58 -0300

    Create Delivery

---
+++ Models/Delivery.cs
@@ -0,0 +1,12 @@
+﻿namespace storeproject.Models
+{
+    public class Delivery
+    {
+        public int Id { get; set; }
+        public int IdRequestItem { get; set; }
+        public int IdDriver { get; set; }
+        public string ? ExpectedDate { get; set; }
+        public string Status { get; set; }
+        public string Observation { get; set; }
+    }
+}
